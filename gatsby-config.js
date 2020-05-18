90a5badde6fed6233a485f3d6d501159de66ff5d
Christie Hui <cwsh15@gmail.com>
2020-05-17 19:43:07 -0700

    initial setup and page layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Stellar by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Stellar by HTML5 UP"
+    title: "DEM LDFS Test",
+    author: "Christie Hui",
+    description: "The LDFS Score Test from Delta Epsilon Mu, Inc."
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'dem-ldfstest',
+        short_name: 'ldfstest',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+        icon: 'src/assets/images/demlogo.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
-import logo from '../assets/images/logo.svg';
+import logo from '../assets/images/demlogo.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
-        <h1>Stellar</h1>
-        <p>Just another free, fully responsive site template<br />
-        design by <a href="https://html5up.net">HTML5 UP</a>.</p>
+        <h1>DEM LDFS Score Test</h1>
+        <p>Take this test to see how LDFS you are (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</p>
     </header>
 )
 
