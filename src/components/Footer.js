da340ddf5a74d04d18e3ecd76bc117788620ed81
Christie Hui <cwsh15@gmail.com>
2020-05-21 16:48:03 -0700

    update formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = props => (
   <footer id="footer">
     <p className="copyright">
-      &copy; <a href="https://christiehui.com" target="_blank" rel="noreferrer">Christie Hui (National Web Developer @ Delta Epsilon Mu, Inc.)</a> and Delta Epsilon Mu, Inc. 2020 (´｡• ω •｡`) ♡
+      &copy; <a href="https://christiehui.com" target="_blank" rel="noreferrer">Christie Hui (National Web Developer @ Delta Epsilon Mu, Inc.)</a> and Delta Epsilon Mu, Inc. 2020.
     </p>
   </footer>
 )
